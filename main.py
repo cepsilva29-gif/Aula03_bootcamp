86411cab7b3ed01121e140a47861bbb1aec19aed
Carlos Eduardo <cesilva29@hotmail.com>
2026-06-25 16:39:40 -0300

    Teste usando o Debug do Python, linha por linha

---
+++ main.py
@@ -0,0 +1,19 @@
+#print("esse e o primeiro comando")
+#x = 5
+#print("esse e o segundo comando")
+#x = 6
+#print("esse e o terceiro comando")
+#print("x ")
+
+x = -3
+
+if x < 0:
+    print("é menor do que zero")
+
+elif x <= 2:
+    print("é menor do que dois")
+
+else:
+    print("More")
+
+
